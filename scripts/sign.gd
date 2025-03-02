fed4a3f5b785e98cfeb67db1d15f6d715a6b1848
jh7gz <jh7gz@umsystem.edu>
2025-03-02 05:15:03 -0600

    Added sign, Added music, Completed Maze

---
+++ scripts/sign.gd
@@ -0,0 +1,15 @@
+extends Area2D
+
+@onready var locations: Label = $Locations
+var inside = false
+
+func _on_body_entered(body: Node2D) -> void:
+	inside = true
+		
+func _on_body_exited(body: Node2D) -> void:
+	inside = false
+	locations.text = ""
+	
+func _shortcut_input(inp):
+	if inp.is_action_pressed("interact") and inside:
+		locations.text = "Lake V\nHouse >\nMaze >"
