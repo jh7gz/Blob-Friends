8b3b266cef17907f8033a4f0906eb23dfafac447
jh7gz <jh7gz@umsystem.edu>
2025-03-01 23:24:26 -0600

    fixed movement

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,19 +1,32 @@
 extends CharacterBody2D
 
 @export var SPEED = 7500
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 func _process(delta):
 	
-	var xdirection = Input.get_axis("ui_left", "ui_right")
+	var xdirection = Input.get_axis("left", "right")
+	var ydirection = Input.get_axis("up", "down")
+	
 	if xdirection:
 		velocity.x = xdirection * SPEED * delta
+		if xdirection > 0:
+			animated_sprite.play("WalkRight")
+		elif xdirection < 0:
+			animated_sprite.play("WalkLeft")
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		
-	var ydirection = Input.get_axis("ui_up", "ui_down")
 	if ydirection:
 		velocity.y = ydirection * SPEED * delta
+		if ydirection > 0:
+			animated_sprite.play("WalkDown")
+		elif ydirection < 0:
+			animated_sprite.play("WalkUp")
 	else:
 		velocity.y = move_toward(velocity.x, 0, SPEED)
+		
+	if !xdirection and !ydirection:
+		animated_sprite.play("Idle")
 		
 	move_and_slide()
