d3f3608fa1c0c112fc83aa459886f881e7507880
jh7gz <jh7gz@umsystem.edu>
2025-03-02 05:44:12 -0600

    Win added

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -6,7 +6,13 @@ var score = 0
 func add_point():
 	score +=  1;
 	friend_count.text = "Friends Found: " + str(score) + "/4"
+	if score == 4:
+		win()
 	
 func _shortcut_input(inp):
 	if inp.is_action_pressed("escape"):
 		get_tree().quit()
+		
+func win():
+	friend_count.text = "Blob!!!\nYOU FOUND YOUR FRIENDS!!!\n\n\n\n\nNow Esc to be\nTogether Forever!"
+	
